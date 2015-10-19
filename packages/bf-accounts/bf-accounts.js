699a169589d19a06c2c12627b097e9423d31ba56
abhijithsj <test.abhijith@gmail.com>
2015-10-19 17:20:49 +0530

    added multi-line address and billforward configurations

--- packages/bf-accounts/bf-accounts.js
+++
@@ -1,9 +0,0 @@
-// Write your package code here!
-
-var model = new WtBillForward.Account();
-
-BillForward.Account.create(model)
-.then(function(account) {
-  console.log(account.toString());
-})
-.catch(console.error);
--- packages/bf-accounts/lib/router.js
+++
@@ -1,9 +0,0 @@
-Router.route('/create', {
-  name: 'create',
-  template: 'wtCreateAccount'
-  
-});
-
-Router.onBeforeAction(function() {
-  this.next();
-});
